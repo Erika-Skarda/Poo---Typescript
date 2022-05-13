44b0c5f67cf386b45ff4848a6910c0cd4a42abab
Erika Skarda <erikaskarda@yahoo.com.br>
2022-05-13 17:38:47 -0300

    metodos, parametros e retorno

--- POO - TypeScript/Personagem.ts
+++ POO - TypeScript/Personagem.ts
@@ -1,58 +1,66 @@
 import prompt from 'prompt-sync'
 
-// Energia, Vida, Ataque, Defesa e o Nome
-class Personagem {
-  // COMO INCLUÍ CONSTRUTOR N PRECISAM SER DEFINIDOS COMO ATRIBUTOS DA CLASSE
-
-  // nome: string = '';
-  // energia: number = 0;
-  // vida: number = 0;
-  // ataque: number = 0;
-  // defesa: number = 0;
-
+export class Personagem {
   constructor(
     public nome: string,
     public energia: number,
     public vida: number,
     public ataque: number,
     public defesa: number
+    
+    ) {}
 
-    ) {
-      // this.nome = nome
-      // this.energia = 50;
-      // this.ataque = 50;
-  }
-}
+    //  TODA VEZ QUE PENSAR EM LER DADOS NA CLASSE - USE PARAMETRO
+    //  TODA VEZ QUE PENSAR EM IMPRIMIR NA CLASSE, USE RETORNO
 
-let teclado = prompt()
-let option: number  = 0;
-while (option !== 9) {
-  console.log("+====== Personagem =======+")
-  console.log("|1. Treinar Ataque        |")
-  console.log("|2. Treinar defesa        |")
-  console.log("|3. Imprimir atributos    |")
-  console.log("|9. Sair                  |")
-  console.log("+========================+")
+  status(): string {
+    return (
+      "Guerreiro: \n" +
+      "\nNome: " +
+      this.nome +
+      ("\nEnergia: " + this.energia.toFixed(1)) +
+      ("\nAtaque: " + this.ataque.toFixed(1)) +
+      ("\nDefesa: " + this.defesa.toFixed(1))
+    ) ;
 
-  option = +teclado("Escolha uma ação")
-
-  switch(option) {
-    case 1:
-      sansa.ataque += 2;
-      break;
-    case 3:
-      console.log("Sansa", sansa)
-      break;
-    default:
-      break
   }
 
+  treinarAtacar(): void {
+    this.ataque += Math.random() * 7;
+    this.energia -= Math.random() * 10;
+    this.isDead()
+    if (this.defesa > 100) {
+      this.defesa = 0;
+    }
+  }
+
+  treinarDefesa(): void {
+    this.defesa += Math.random() * 5;
+    this.energia -= Math.random() * 10;
+    if (this.defesa > 100) {
+      this.defesa = 0;
+    }
+  } 
+
+  descansar(hour: number): void {
+    this.energia += hour * (Math.random() * 10);
+    if (this.energia > 100) {
+        this.energia = 100;
+    }
+  }
+
+  batalhar(): number {
+    let desgaste: number = Math.random() * 10;
+    this.energia -= desgaste; 
+    return desgaste;
+  }
+
+  isDead(): boolean {
+    return (this.energia < 0) 
+  }
 }
-let sansa: Personagem;
-sansa = new Personagem('Sansa Stark', 100, 40, 20, 20);
-// sansa.ataque = 10;
-// sansa.defesa = 20;
-// sansa.energia = 50;
-// sansa.vida = 100;
-console.log(sansa);
+  
+
+  
+
 
